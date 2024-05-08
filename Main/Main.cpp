0430e0da7c7fccdfca73b508afeef1e84a88c947
Arnoldas Stasiūnas <arnoldas.stasiunas@stud.vilniustech.lt>
2024-05-08 23:58:52 +0300

    Added Menu

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <Windows.h>
+#include <conio.h>
 using namespace std;
 
 #define RESET "\033[0m" //Reset to DEFAULT color
@@ -15,13 +17,14 @@ using namespace std;
 
 
 void DrawTitle();
+void Menu();
 
 int main()
 {
 	DrawTitle();
+	Menu();
 
-	cout << "hello";
-	cout << "hello";
+	//GOLD HUNTER
 
 	return 0;
 }
@@ -44,4 +47,88 @@ void DrawTitle()
 	cout << YELLOW << "**" << RESET << BROWN << "   +++++++++++  " << RESET << GREEN << "$$$$$$$$$  $$$   $$$  $$$  $$$" << RESET << GREY << " |*" << RESET << RED << "X" << RESET << GREY << "*| " << RESET << BROWN << " +++++++++++    " << RESET << YELLOW << "**" << RESET << endl;
 	cout << YELLOW << "**" << RESET << BLUE << "  ~~~~~~~~~~~~~ " << RESET << CYAN << "$$$$$$$$$  $$$   $$$  $$$  $$$" << RESET << GREY <<"   * " << RESET << BLUE << "  ~~~~~~~~~~~~~   " << RESET << YELLOW << "**" << RESET << endl;
 	cout << YELLOW << " ********************************************************************* " << RESET << endl;
+}
+
+void Menu()
+{
+	bool exitMenu = false;
+	int selectedOption = 0;
+	char keyPressed = ' ';
+
+	while (!exitMenu)
+	{
+		system("cls");
+		DrawTitle();
+		cout << endl;
+
+		if (selectedOption == 0)
+		{
+			cout << GREY << "                              Play a Game                              " << RESET << endl;
+			cout << "                                 Rules                                 " << endl;
+			cout << "                                 ABOUT                                 " << endl;
+		}
+		else if (selectedOption == 1)
+		{
+			cout << "                              Play a Game                              " << endl;
+			cout << GREY << "                                 Rules                                 " << RESET << endl;
+			cout << "                                 ABOUT                                 " << endl;
+		}
+		else
+		{
+			cout << "                              Play a Game                              " << endl;
+			cout << "                                 Rules                                 " << endl;
+			cout << GREY << "                                 ABOUT                                 " << RESET << endl;
+		}
+
+		cout << endl;
+		cout << BLUE << "SELECTED OPTION (MENU): " << RESET << selectedOption + 1 << endl << endl;
+
+		while (true)
+		{
+			if (_kbhit())
+			{
+				keyPressed = _getch();
+				switch (keyPressed)
+				{
+				case 72: //Move up
+					if (selectedOption > 0) {
+						selectedOption--;
+					}
+					else if (selectedOption == 0) {
+						selectedOption += 2;
+					}
+					break;
+				case 80: //Move down
+					if (selectedOption < 2) {
+						selectedOption++;
+					}
+					else if (selectedOption == 2) {
+						selectedOption -= 2;
+					}
+					break;
+				}
+				break;
+			}
+		}
+
+		if (GetAsyncKeyState(VK_RETURN))
+		{
+			switch (selectedOption)
+			{
+			case 0:
+				//system("cls");
+				cout << "Starting the game..." << endl;
+				break;
+			case 1:
+				//system("cls");
+				cout << "Displaying the rules..." << endl;
+				break;
+			case 2:
+				//system("cls");
+				cout << "Showing about..." << endl;
+				break;
+			}
+			exitMenu = true;
+		}
+	}
 }
